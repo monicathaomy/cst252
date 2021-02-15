bf3e4377ff854dd3a22bc99c3cf36f641354e3b7
monicathaomy <70286121+monicathaomy@users.noreply.github.com>
2021-02-14 21:21:08 -0800

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,26 @@
+/*
+* Author: Monica To
+* Created: February 14, 2021
+* License: Public Domain
+*/
+
+// Declare variables to hold the make, model, color, and year of your vehicle
+var make = "Lexus";
+
+var model = "GS 350";
+
+var color = "navy";
+
+var year = 2008;
+//Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+//Create a new variable age that subtracts the year from the current year
+
+var age = 2021 - year;
+
+//Use document.writeln() to neatly output each of your variables
+document.writeln("Model: "+ model +"<br>");
+document.writeln("Make: "+ make + "<br>");
+document.writeln("Color: "+ color +"<br>");
+document.writeln("Year": "+ year +"<br>");
+document.writeln("Age: "+age +"<br>");
