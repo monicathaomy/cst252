f40f7206cf1f3b37465b3084eb58c704f7b82d9d
monicathaomy <70286121+monicathaomy@users.noreply.github.com>
2021-02-16 21:33:12 -0800

    lab6

---
+++ lab6/js/lab6.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Monica To
+ * Created: February 16, 2021
+ * License: Public Domain
+ */
+
+ //Declare an array myTransportation that lists all of the forms of transportation you use to get around.
+ var myTransportation = ["Lexus GS350", "rides from friends" , "Lyft"];
+ //Declare an object myMainRide that collectts all the data you specitied for your primary vehiclein Lab 5.
+ var myMainRide = {
+   make: "Lexus",
+   model: "GS350",
+   color: "navy",
+   year: 2008
+   age: function() {
+     return 2021-age;
+   }
+
+ }
+ //Use document.writeln() to neatly output myTransportation
+ document.writeln("My types of transportation: " + myTransport + "<br>");
+ //Output myMainRide
+ document.writeln("My main ride <pre>",
+      JSON.stringify(myMainRide, null, '\t'), "</pre>");
